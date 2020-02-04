7d45b3eba7d08ab2ae901eb86eb6adf724f6dd3e
Martin Bagic <usr@DESKTOP-7G12JDI.localdomain>
2020-02-04 19:19:11 +0100

    fix static files

--- backend/viz/views.py
+++ backend/viz/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def main_visualization(request):
-    render(request,'viz/index.html',)
+    return render(request,'viz/index.html',)
