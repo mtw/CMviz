6b971bd908e81c4ad056da624fac7f574bb78509
Martin Bagic <usr@DESKTOP-7G12JDI.localdomain>
2020-02-04 21:31:33 +0100

    remove inspiration code

--- CMviz/CMviz/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from . import views
-
-
-def home(request):
-    return render(request, 'CMviz/home.html')
--- CMviz/upload_file/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UploadFileConfig(AppConfig):
-    name = 'upload_file'
