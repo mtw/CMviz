892409d66d65f3061b9db6c5601a29f262c23e3b
Martin Bagic <martin.bagic@outlook.com>
2020-02-29 18:03:55 +0100

    add static files in upload_file app

--- CMviz/settings.py
+++ CMviz/settings.py
@@ -95,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 
 # Internationalization
@@ -126,5 +126,5 @@ STATIC_URL = "/static/"
 STATIC_DIR = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "viz", "static"),
+    os.path.join(BASE_DIR, "upload_file", "static"),
 ]
-
