b2172adc8086e7572ce5dc2da87aabd761ad597e
Martin Bagic <martin.bagic@outlook.com>
2020-06-25 15:41:24 +0200

    Add ViennaRNA absolute paths

--- root/viz/transformers/imports.py
+++ root/viz/transformers/imports.py
@@ -3,6 +3,8 @@ import sys
 paths_RNA = (
     r'M:\projects-active\uniW19\ue-software-dev-backup\ViennaRNA-2.4.14\interfaces\Python3', # Win
     r'/mnt/m/projects-active/uniW19/ue-software-dev-backup/ViennaRNA-2.4.14/interfaces/Python3', # Linux
+    r'M:\projects-active\UE softdev\ViennaRNA-2.4.14\interfaces\Python3',
+    r'/mnt/m/projects-active/UE softdev/ViennaRNA-2.4.14/interfaces/Python3',
 )
 
 
